e9ddc7bab3671aca48b9dfc434b3b8fb327c4eef
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2024-09-25 23:01:17 -0400

    big update

--- app/components/WelcomeMessage.tsx
+++ app/components/WelcomeMessage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WelcomeMessage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center">
-      <h1 className="text-6xl font-bold mb-4 text-black">helix.</h1>
+      <h1 className="text-6xl font-bold mb-4 text-black">Helix.</h1>
       <p className="text-xl text-black">Creating memorable connections.</p>
     </div>
   );
