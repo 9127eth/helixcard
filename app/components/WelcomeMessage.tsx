bc0688e8a3d220c3a9c03049739def2186e7e6fe
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2024-09-26 02:09:39 -0400

    undoing redirect

--- app/components/WelcomeMessage.tsx
+++ app/components/WelcomeMessage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WelcomeMessage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center">
-      <h1 className="text-6xl font-bold mb-4 text-black">Helix.</h1>
+      <h1 className="text-6xl font-bold mb-4 text-black">Helix</h1>
       <p className="text-xl text-black">Helping people create memorable connections.</p>
     </div>
   );
