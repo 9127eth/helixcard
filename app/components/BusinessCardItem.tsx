f64bb375573afd2b3b7a0e9ec3597f5b173b165c
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2024-09-29 23:10:31 -0400

    fixed card sizes for grid

--- app/components/BusinessCardItem.tsx
+++ app/components/BusinessCardItem.tsx
@@ -14,7 +14,7 @@ export const BusinessCardItem: React.FC<BusinessCardItemProps> = ({ card, onView
   const [isShareModalOpen, setIsShareModalOpen] = useState(false);
 
   return (
-    <div className="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow relative h-auto flex flex-col justify-between bg-card-grid-background">
+    <div className="w-full bg-card-grid-background border rounded-lg shadow-sm hover:shadow-md transition-shadow relative flex flex-col p-4 h-[180px]">
       {card.isPrimary && (
         <div className="absolute top-2 right-2 group">
           <FaStar className="h-3 w-3 text-black" />
@@ -23,14 +23,14 @@ export const BusinessCardItem: React.FC<BusinessCardItemProps> = ({ card, onView
           </span>
         </div>
       )}
-      <div>
-        <h3 className="text-3xl font-semibold mb-2 line-clamp-2 overflow-hidden">
+      <div className="flex-grow overflow-hidden">
+        <h3 className="text-3xl font-semibold mb-2 line-clamp-1 overflow-hidden">
           {card.description}
         </h3>
-        <p className="text-sm text-gray-600">{card.jobTitle}</p>
-        <p className="text-sm text-gray-600">{card.company}</p>
+        <p className="text-sm text-gray-600 truncate">{card.jobTitle}</p>
+        <p className="text-sm text-gray-600 truncate">{card.company}</p>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mt-4 space-y-4 md:space-y-0">
+      <div className="mt-auto">
         <div className="flex space-x-2">
           <Link href={`/edit-card/${card.id}`} className="card-grid-icon-button group relative">
             <FaEdit />
--- app/components/CreateCardButton.tsx
+++ app/components/CreateCardButton.tsx
@@ -10,7 +10,7 @@ export const CreateCardButton: React.FC = () => {
   }
 
   return (
-    <Link href="/create-card" className="flex flex-col items-center justify-center h-full bg-white text-gray-800 rounded-lg shadow-md hover:shadow-lg transition-shadow border-2 border-[#FECAB9]">
+    <Link href="/create-card" className="w-full bg-white text-gray-800 rounded-lg shadow-sm hover:shadow-md transition-shadow border-2 border-[#FECAB9] flex flex-col items-center justify-center p-4 h-[180px]">
       <div className="text-3xl mb-2">+</div>
       <div className="font-bold text-xl">Create New</div>
     </Link>
